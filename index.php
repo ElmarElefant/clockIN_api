fbff6c355a847c6dc5e3d2b572cbd539f90c6b63
ElmarElefant <github@rolandwehrli.ch>
2024-03-25 16:28:36 +0100

    try 2

--- index.php
+++ index.php
@@ -71,7 +71,6 @@ header("Access-Control-Allow-Headers: content-type, authorization");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 
 
-echo "Hallo";
 
 // Ressourcen laden.
 require __DIR__ . "/includes/bootstrap.inc.php";
@@ -101,6 +100,7 @@ $payload = $contentType == "application/json" ? json_decode( file_get_contents('
 
 
 
+echo "Hallo";
 echo $_GET['endpoint'];
 
 
